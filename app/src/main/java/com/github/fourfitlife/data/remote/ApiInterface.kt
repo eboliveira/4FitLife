1bfef97d38aea6473212664bb4c803087ce84c9e
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-03 13:24:57 +0000

    fix(api): use query parameter correctly

--- app/src/main/java/com/github/fourfitlife/data/remote/ApiInterface.kt
+++ app/src/main/java/com/github/fourfitlife/data/remote/ApiInterface.kt
@@ -3,10 +3,10 @@ package com.github.fourfitlife.data.remote
 import com.github.fourfitlife.data.models.UserExercise
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 
 interface ApiInterface {
-    @GET("day/{userId}")
-    fun getUserExercises(@Path("userId") userId: String): Call<List<UserExercise>>
+    @GET("day")
+    fun getUserExercises(@Query("userId") userId: String): Call<List<UserExercise>>
 }
