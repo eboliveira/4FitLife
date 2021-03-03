c4503652a08182671626f6c7460f1cdcab31786f
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-03 12:28:01 +0000

    feat(models): added user exercise model

---
+++ app/src/main/java/com/github/fourfitlife/ui/data/models/UserExercise.kt
@@ -0,0 +1,18 @@
+package com.github.fourfitlife.ui.data.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity
+@Serializable
+data class UserExercise(
+    @PrimaryKey val id: String,
+
+    val exercise: Exercise,
+
+    val repetitions: Int?,
+
+    @ColumnInfo(name = "seconds_time") val secondsTime: Int?
+)
