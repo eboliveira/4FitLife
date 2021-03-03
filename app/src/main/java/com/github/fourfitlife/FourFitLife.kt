31c3bcdc775dc03550fc4df3d94d63a7916f6d69
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-03 13:03:19 +0000

    feat(application): added application class

---
+++ app/src/main/java/com/github/fourfitlife/FourFitLife.kt
@@ -0,0 +1,10 @@
+package com.github.fourfitlife
+
+import android.app.Application
+import com.github.fourfitlife.helpers.SharedPreferencesHelper
+
+class FourFitLife: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
