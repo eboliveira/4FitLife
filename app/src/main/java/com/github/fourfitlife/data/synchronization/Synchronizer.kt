ecabbb7afa104e016a713164ed26554f7c24df01
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-04 22:18:21 +0000

    feat(sync): added synchronizer class

---
+++ app/src/main/java/com/github/fourfitlife/data/synchronization/Synchronizer.kt
@@ -0,0 +1,31 @@
+package com.github.fourfitlife.data.synchronization
+
+import com.github.fourfitlife.FourFitLife
+import com.github.fourfitlife.data.DatabaseInterface
+import com.github.fourfitlife.data.daos.UserExerciseDao
+import com.github.fourfitlife.data.models.UserExercise
+import com.github.fourfitlife.data.remote.Api
+import com.github.fourfitlife.helpers.SharedPreferencesHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class Synchronizer {
+    companion object {
+        suspend fun run() = withContext(Dispatchers.IO) {
+            val userId = SharedPreferencesHelper.userId
+            userId ?: return@withContext
+            val call = Api.service.getUserExercises(userId).execute()
+            if (call.isSuccessful) {
+                val userExercises = call.body()
+                if (userExercises == null || userExercises.isEmpty())
+                    return@withContext
+
+                FourFitLife.db.userExerciseDao().clean()
+                FourFitLife.db.userExerciseDao().insertAll(userExercises)
+                SynchronizerInterface.notify(true)
+            } else {
+                SynchronizerInterface.notify(false)
+            }
+        }
+    }
+}
