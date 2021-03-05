6e7257e1b8e23fd1270e2b40885cafb0d49cba09
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-05 18:41:50 +0000

    feat(day): set fragment to use data binding

--- app/src/main/java/com/github/fourfitlife/ui/day/DayFragment.kt
+++ app/src/main/java/com/github/fourfitlife/ui/day/DayFragment.kt
@@ -4,28 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.fourfitlife.R
+import com.github.fourfitlife.databinding.FragmentDayBinding
 
 class DayFragment : Fragment() {
-
     private lateinit var dayViewModel: DayViewModel
+    private lateinit var viewModelAdapter: UserExerciseAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        dayViewModel =
-                ViewModelProvider(this).get(DayViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_day, container, false)
-        val textView: TextView = root.findViewById(R.id.text_day)
-        dayViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding: FragmentDayBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_day,
+            container,
+            false)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        dayViewModel = ViewModelProvider(this).get(DayViewModel::class.java)
+        binding.viewModel = dayViewModel
+        viewModelAdapter = UserExerciseAdapter()
+        binding.root.findViewById<RecyclerView>(R.id.user_exercise_recycler_view).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = viewModelAdapter
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dayViewModel.userExercises.observe(viewLifecycleOwner, Observer { userExercises ->
+            userExercises?.apply {
+                viewModelAdapter.userExercises = userExercises
+            }
         })
-        return root
     }
 }
