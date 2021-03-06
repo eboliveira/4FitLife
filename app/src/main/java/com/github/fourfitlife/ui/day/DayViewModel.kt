ef24ca726478ae657c5a6a4bfba8aebd21a2fac2
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-03-06 18:34:04 +0000

    refactor(test): change day tests to work even running all

--- app/src/main/java/com/github/fourfitlife/ui/day/DayViewModel.kt
+++ app/src/main/java/com/github/fourfitlife/ui/day/DayViewModel.kt
@@ -8,5 +8,5 @@ import com.github.fourfitlife.data.repositories.UserExerciseRepository
 // TODO Refactor this to be dynamic and can be used by other classes. It requires to implement Factory
 // https://developer.android.com/codelabs/kotlin-android-training-view-model#7
 class DayViewModel : ViewModel() {
-    val userExercises: LiveData<List<UserExercise>> = UserExerciseRepository.userExercises
+    val userExercises = UserExerciseRepository.userExercises
 }
